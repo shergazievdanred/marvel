428a8f63615ebf7432203108b65b68e2b9332636
Daniyar <shergazievdan@gmail.com>
2022-02-22 20:24:20 +0600

    marvel-tab-22.02.2022

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -6,6 +6,8 @@ import CharList from "../charList/CharList";
 import CharInfo from "../charInfo/CharInfo";
 import ErrorBoundary from '../errorBoundary/ErrorBoundary';
 
+
+
 import decoration from '../../resources/img/vision.png';
 
 class App extends Component {
@@ -42,5 +44,7 @@ class App extends Component {
         )
    }
 }
+
+
 
 export default App;
--- src/components/charInfo/CharInfo.js
+++ src/components/charInfo/CharInfo.js
@@ -1,5 +1,6 @@
 
 import {Component} from 'react';
+import PropTypes from 'prop-types';
 
 import MarvelService from '../../services/MarvelService';
 import Spinner from '../spinner/Spinner';
@@ -130,6 +131,10 @@ const View = ({char}) => {
             </ul>
         </>
     )
+}
+
+CharInfo.propTypes = {
+    charId: PropTypes.number
 }
 
 export default CharInfo;
